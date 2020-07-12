426b842d36c906809557804b5d810dc51c38f5eb
Ryan <ryan@commercialprogression.com>
2020-07-12 19:04:17 -0400

    spacing tweaks, content, etc.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,11 @@ var dark_toggle = '';
 
 var profile = document.getElementById('profile');
 var link_toggle = document.getElementById('dark-toggle');
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.getElementById('top').classList.toggle("dark-mode");
+}
+
 profile.addEventListener("click", function(){
     document.getElementById('top').classList.toggle("dark-mode");});
 link_toggle.addEventListener("click", function(){
