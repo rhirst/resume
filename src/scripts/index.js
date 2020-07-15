52a23d1a31d9e2ebd871ff87142bff8faa28406d
Ryan <ryan@commercialprogression.com>
2020-07-15 19:32:18 -0400

    lighthouse tweaks, got rid of silly header slider

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,14 +2,15 @@ import list from './modules/list'
 
 var dark_toggle = '';
 
-var profile = document.getElementById('profile');
+var face = document.getElementById('face');
 var link_toggle = document.getElementById('dark-toggle');
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.getElementById('top').classList.toggle("dark-mode");
+    link_toggle.innerHTML = 'light mode';
 }
 
-profile.addEventListener("click", function(){
+face.addEventListener("click", function(){
     document.getElementById('top').classList.toggle("dark-mode");});
 link_toggle.addEventListener("click", function(){
     document.getElementById('top').classList.toggle("dark-mode");
